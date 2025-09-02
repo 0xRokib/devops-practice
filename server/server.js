e760fcc1ee304bea7fa092fc3952e66295c5422e
0xRokib <rokibulhasan.ph@gmail.com>
2025-09-02 21:34:54 +0600

    dev branch console changes

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ mongoose
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
-  console.log('Changed again to - Shutting down gracefully...');
+  console.log('Changed Branch to Dev - Shutting down gracefully...');
   await mongoose.connection.close();
   process.exit(0);
 });
